ba09fba98a819aad81e506239fc903662b55b690
mohamed00Sayed <sayedm22529@gmail.com>
2021-11-10 17:52:06 +0200

    setting multipart config

--- src/main/java/com/sayed/netter/config/NetterWebInitializer.java
+++ src/main/java/com/sayed/netter/config/NetterWebInitializer.java
@@ -1,24 +1,33 @@
 package com.sayed.netter.config;
 
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.sayed.netter.web.WebConfig;
 
 public class NetterWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-  
-  @Override
-  protected Class<?>[] getRootConfigClasses() {
-    return new Class<?>[] { RootConfig.class };
-  }
 
-  @Override
-  protected Class<?>[] getServletConfigClasses() {
-    return new Class<?>[] { WebConfig.class };
-  }
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { RootConfig.class };
+	}
 
-  @Override
-  protected String[] getServletMappings() {
-    return new String[] { "/" };
-  }
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setMultipartConfig(new MultipartConfigElement("/tmp/netter/uploads"));
+	}
 
 }
